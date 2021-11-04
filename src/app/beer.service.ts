7022c467d63b958578d012ca77c6f1a9fe5a575a
SandraT <sndrbltr@gmail.com>
2021-11-04 18:37:48 +0000

    messages

--- src/app/beer.service.ts
+++ src/app/beer.service.ts
@@ -18,7 +18,7 @@ export class BeerService {
   getBeers(sort?:string|null): Observable<Beer[]> {
     const beers = of(BEERS);
     
-    //this.messageService.add('fetched');
+    this.messageService.add('fetched');
     return beers.pipe(map((beers:Beer[])=>{
       if (!sort) return beers;
       const tmpBeers:Beer[]=[];
@@ -34,7 +34,7 @@ export class BeerService {
   getBeer(id: number): Observable<Beer> {
     // For now, assume that a beer with the specified `id` always exists.
      const beer = BEERS.find(b => b.id === id)!;
-    //this.messageService.add(`fetched id: ${id}`);
+    this.messageService.add(`fetched id: ${id}`);
     return of(beer);
   }
 }
